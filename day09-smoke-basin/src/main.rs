204383d6606d1f1648f305b13875cf9b52614993
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-12-19 22:43:06 +0000

    Day 9-2: Find largest basins

--- day09-smoke-basin/src/main.rs
+++ day09-smoke-basin/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashSet, VecDeque, BinaryHeap};
 use shared::{file_lines, read_first_arg, MyError};
 
 type Point = (usize, usize);
@@ -14,6 +15,9 @@ fn main() -> Result<(), MyError> {
     let risk = calculate_risk_level(&input);
     println!("Rist level: {}", risk);
 
+    let largest_basins = find_largest_basins(&input);
+    println!("Largest basins: {}", largest_basins);
+
     Ok(())
 }
 
@@ -22,6 +26,50 @@ fn calculate_risk_level(heightmap: &Vec<Vec<u32>>) -> u32 {
         .iter()
         .map(|(row_idx, col_idx)| &heightmap[*row_idx][*col_idx] + 1)
         .sum()
+}
+
+fn find_largest_basins(heightmap: &Vec<Vec<u32>>) -> u32 {
+    let mut heap = BinaryHeap::new();
+
+    get_low_points(heightmap)
+        .iter()
+        .map(|&p| calculate_basin_size(heightmap, p))
+        .for_each(|size| heap.push(size));
+
+    (0..3)
+        .map(|_| heap.pop().unwrap_or(1))
+        .product()
+}
+
+fn calculate_basin_size(heightmap: &Vec<Vec<u32>>, start: Point) -> u32 {
+    let max_row = heightmap.len() - 1;
+    let max_col = heightmap[0].len() - 1;
+    let get_adj = |p: Point| get_adj_idx(max_row, max_col, p.0, p.1);
+
+    let mut queue = VecDeque::new();
+    queue.push_back(start);
+
+    let mut visited: HashSet<Point> = HashSet::new();
+
+    let mut size = 0;
+    while !queue.is_empty() {
+        let point = queue.pop_front().unwrap();
+        visited.insert(point);
+        size += 1;
+
+        get_adj(point)
+            .iter()
+            .filter(|&p| {
+                heightmap[p.0][p.1] != 9 && heightmap[p.0][p.1] > heightmap[point.0][point.1]
+            })
+            .for_each(|&p| {
+                if !visited.contains(&p) {
+                    visited.insert(p);
+                    queue.push_back(p);
+                }
+            });
+    }
+    size
 }
 
 fn get_low_points(heightmap: &Vec<Vec<u32>>) -> Vec<Point> {
@@ -93,6 +141,54 @@ mod tests {
 
         let expected = 15;
         let actual = calculate_risk_level(&input);
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_basin_size() {
+        let heightmap: Vec<Vec<u32>> = "
+            2199943210
+            3987894921
+            9856789892
+            8767896789
+            9899965678"
+            .split_ascii_whitespace()
+            .filter(|&s| !s.is_empty())
+            .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .collect();
+
+        let expected = 3;
+        let actual = calculate_basin_size(&heightmap, (0, 1));
+        assert_eq!(expected, actual);
+
+        let expected = 9;
+        let actual = calculate_basin_size(&heightmap, (0, 9));
+        assert_eq!(expected, actual);
+
+        let expected = 14;
+        let actual = calculate_basin_size(&heightmap, (2, 2));
+        assert_eq!(expected, actual);
+
+        let expected = 9;
+        let actual = calculate_basin_size(&heightmap, (4, 6));
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_find_largest_basins() {
+        let heightmap: Vec<Vec<u32>> = "
+            2199943210
+            3987894921
+            9856789892
+            8767896789
+            9899965678"
+            .split_ascii_whitespace()
+            .filter(|&s| !s.is_empty())
+            .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect())
+            .collect();
+
+        let expected = 1134;
+        let actual = find_largest_basins(&heightmap);
         assert_eq!(expected, actual);
     }
 }
