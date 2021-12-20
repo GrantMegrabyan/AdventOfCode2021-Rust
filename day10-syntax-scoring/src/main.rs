d4eda89ca29a412fa29409d4af6dbb9959d7418c
Grant Megrabyan <grant.megrabyan@gmail.com>
2021-12-20 23:22:41 +0000

    Day 10-2: Calculate scores for incomplete lines

--- day10-syntax-scoring/src/main.rs
+++ day10-syntax-scoring/src/main.rs
@@ -9,10 +9,14 @@ use std::{
 };
 
 lazy_static! {
-    static ref BRACKETS: HashMap<char, char> =
+    static ref CLOSE_TO_OPEN: HashMap<char, char> =
         HashMap::<_, _>::from_iter([(')', '('), (']', '['), ('}', '{'), ('>', '<'),]);
-    static ref SCORES: HashMap<char, u32> =
+    static ref OPEN_TO_CLOSE: HashMap<char, char> =
+        HashMap::<_, _>::from_iter([('(', ')'), ('[', ']'), ('{', '}'), ('<', '>'),]);
+    static ref CORRUPT_SCORES: HashMap<char, u32> =
         HashMap::<_, _>::from_iter([(')', 3), (']', 57), ('}', 1197), ('>', 25137),]);
+    static ref INCOMPLETE_SCORES: HashMap<char, u64> =
+        HashMap::<_, _>::from_iter([(')', 1), (']', 2), ('}', 3), ('>', 4),]);
 }
 
 fn main() -> Result<(), MyError> {
@@ -22,6 +26,10 @@ fn main() -> Result<(), MyError> {
     let total_score = calculate_total_syntax_error_score(reader);
     println!("Total syntax error score: {}", total_score);
 
+    let reader = BufReader::new(File::open(file_path.clone())?);
+    let incomplete_middle_score = calculate_incomplete_middle_score(reader);
+    println!("Incomplete middle score: {}", incomplete_middle_score);
+
     Ok(())
 }
 
@@ -29,16 +37,16 @@ fn calculate_total_syntax_error_score<R: Read>(reader: BufReader<R>) -> u32 {
     reader
         .lines()
         .filter_map(|line| get_first_illegal_char(&line.unwrap()))
-        .map(|ch| SCORES[&ch])
+        .map(|ch| CORRUPT_SCORES[&ch])
         .sum()
 }
 
 fn get_first_illegal_char(line: &str) -> Option<char> {
     let mut stack = VecDeque::new();
     for ch in line.chars() {
-        if BRACKETS.contains_key(&ch) {
+        if CLOSE_TO_OPEN.contains_key(&ch) {
             match stack.pop_front() {
-                Some(prev) if prev != BRACKETS[&ch] => return Some(ch),
+                Some(prev) if prev != CLOSE_TO_OPEN[&ch] => return Some(ch),
                 None => return Some(ch),
                 _ => (),
             }
@@ -47,6 +55,38 @@ fn get_first_illegal_char(line: &str) -> Option<char> {
         }
     }
     None
+}
+
+fn calculate_incomplete_middle_score<R: Read>(reader: BufReader<R>) -> u64 {
+    let mut scores = reader
+        .lines()
+        .filter_map(|line| get_completion_string(&line.unwrap()))
+        .map(|s| s.chars().fold(0, |acc, ch| acc * 5 + INCOMPLETE_SCORES[&ch]))
+        .collect::<Vec<_>>();
+
+    scores.sort_unstable();
+    scores[scores.len() / 2]
+}
+
+fn get_completion_string(line: &str) -> Option<String> {
+    let mut stack = VecDeque::new();
+    for ch in line.chars() {
+        if CLOSE_TO_OPEN.contains_key(&ch) {
+            match stack.pop_front() {
+                Some(prev) if prev != CLOSE_TO_OPEN[&ch] => return None,
+                None => return None,
+                _ => (),
+            }
+        } else {
+            stack.push_front(ch);
+        }
+    }
+
+    if stack.is_empty() {
+        return None;
+    }
+
+    Some(stack.iter().map(|ch| OPEN_TO_CLOSE[ch]).collect())
 }
 
 #[cfg(test)]
@@ -87,6 +127,39 @@ mod tests {
 
         let expected = Some(')');
         let actual = get_first_illegal_char("[[<[([]))<([[{}[[()]]]");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_get_completion_string() {
+        let expected = Some(String::from("}}]])})]"));
+        let actual = get_completion_string("[({(<(())[]>[[{[]{<()<>>");
+        assert_eq!(expected, actual);
+
+        let expected = Some(String::from(")}>]})"));
+        let actual = get_completion_string("[(()[<>])]({[<{<<[]>>(");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_calculate_incomplete_middle_score() {
+        let input = r#"
+[({(<(())[]>[[{[]{<()<>>
+[(()[<>])]({[<{<<[]>>(
+{([(<{}[<>[]}>{[]{[(<()>
+(((({<>}<{<{<>}{[]{[]{}
+[[<[([]))<([[{}[[()]]]
+[{[{({}]{}}([{[{{{}}([]
+{<[[]]>}<{[{[{[]{()[[[]
+[<(<(<(<{}))><([]([]()
+<{([([[(<>()){}]>(<<{{
+<{([{{}}[<[[[<>{}]]]>[]]
+        "#;
+        let cursor = std::io::Cursor::new(input.trim());
+        let reader = BufReader::new(cursor);
+
+        let expected = 288957;
+        let actual = calculate_incomplete_middle_score(reader);
         assert_eq!(expected, actual);
     }
 }
